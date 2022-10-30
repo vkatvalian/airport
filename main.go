2f7cd3eb703f16bf84ef8ff28aef2443edfcb2d8
vkatvalian <kamee@progart@protonmail.com>
2022-10-31 01:29:06 +0400

    add database connection

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
     "fmt"
+    "log"
+    "os"
     "net/http"
+    "gorm.io/gorm"
+    "gorm.io/driver/mysql"
+    "github.com/joho/godotenv"
     "github.com/gin-gonic/gin"
 )
+
+var db *gorm.DB
 
 func feed(c *gin.Context){
     username := c.PostForm("username")
@@ -27,6 +34,20 @@ func index(c *gin.Context){
 }
 
 func main(){
+    var err error
+
+    err = godotenv.Load()
+    if err != nil {
+      log.Fatal("Error loading .env file")
+    }
+
+    dsn := os.Getenv("DSN")
+    db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+	fmt.Println(err)
+        panic("failed to connect database")
+    }
+
     router := gin.Default()
     router.LoadHTMLGlob("tmpl/*")
     router.GET("/signup", signup)

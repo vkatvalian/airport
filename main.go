eb4d8456a037f62467e2f2b91da6c6a11aa2f7d2
vkatvalian <kamee@progart@protonmail.com>
2022-10-31 16:29:40 +0400

    insert to db

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "log"
     "os"
+    "time"
     "net/http"
     "gorm.io/gorm"
     "gorm.io/driver/mysql"
@@ -13,12 +14,28 @@ import (
 
 var db *gorm.DB
 
+type Users struct {
+    ID        uint      `gorm:"primaryKey"`
+    Username  string	`gorm:"not null"`
+    Email     string	`gorm:"not null"`
+    Password  string	`gorm: "not null"`
+    CreatedAt time.Time
+    UpdatedAt time.Time
+}
+
 func feed(c *gin.Context){
     username := c.PostForm("username")
     email := c.PostForm("email")
     password := c.PostForm("password")
-    fmt.Println(username, email, password)
 
+    db.AutoMigrate(&Users{})
+    user := &Users{
+        Username: username,
+	Email: email,
+	Password: password,
+    }
+
+    db.Create(&user)
     c.JSON(http.StatusOK, gin.H{
 	"status":  "success",
         "message": username,
@@ -47,6 +64,8 @@ func main(){
 	fmt.Println(err)
         panic("failed to connect database")
     }
+
+    db.AutoMigrate(&Users{})
 
     router := gin.Default()
     router.LoadHTMLGlob("tmpl/*")
